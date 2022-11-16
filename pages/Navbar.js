98fa09cbc38b4b9e40ab7e28fbb0f5a5b850e480
FILAN MAULA ANDINI <filanmaulaandini@MacBook-Air-FILAN.local>
2022-11-16 11:15:35 +0700

    aaa

--- pages/Navbar.js
+++ pages/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
                 </ul>
             </div>
         </nav>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" ></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
         <script src="js/scripts.js"></script>
     </body>
